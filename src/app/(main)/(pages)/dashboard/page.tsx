3b7784d87840ccbadcb4f863f44b7fec0e543afb
TomasHermosillaa <tomas.hermosilla.p@gmail.com>
2024-06-10 16:23:08 -0400

    primera web

---
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+  return (
+    <div className='flex flex-col gap-4 relative'>dashboard</div>
+  )
+}
+
+export default DashboardPage
